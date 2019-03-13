e79d25423f5cb081162c61f0c9616de7d0e3054d
Sea for Mac <18912387311@163.com>
2019-03-13 22:21:41 +0800

    路由

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var http=require('http');
+var url=require('url');
+var route=require('./route')
+
 var service=require('./service/service.js');
 var user=require('./models/person');
 var teacher=require('./models/teacher');
@@ -7,6 +10,9 @@ http.createServer(function(request,response){
 	response.writeHead(200,{'Content-Type':'text/html'});
 	//防止执行2次
 	if(request.url!='/favicon.ico'){
+		var pathname=url.parse(request.url).pathname.replace(/\//,'');
+		route[pathname]();
+
 		service.funciton2(response);
 		service.funciton3(response);
 		tea=new teacher(2,'王海的老师',50);
---
+++ route.js
@@ -0,0 +1,9 @@
+module.exports={
+    login:function(){
+        console.log('login')
+    },
+    logout:function(){
+        console.log('logout');
+    }
+
+}
