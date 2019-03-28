ebee6e50ec75c16cf3a3215bb54f4c1fe73931c7
Sea for Mac <18912387311@163.com>
2019-03-28 20:09:14 +0800

    模拟express

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 const http=require('http');
 const url=require('url');
-const route=require('./route');
 const process=require('process');
+
+const route=require('./route');
+const myExpress=require('./myExpress');
 
 process.on('uncaughtException',e=>{
 	console.log('未捕获的异常：'+e.toString());
 });
 
-var app=http.createServer((request,response)=>{
-	//防止执行2次
-	if(request.url!='/favicon.ico'){
-		var pathname=url.parse(request.url).pathname.replace(/\//,'');
-		try{
-			route[pathname](request,response);
-		}catch(error){
-			response.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
-			response.write(error.toString());
-			response.end();
-		}
-	}
+// var app=http.createServer((request,response)=>{
+// 	//防止执行2次
+// 	if(request.url!='/favicon.ico'){
+// 		var pathname=url.parse(request.url).pathname.replace(/\//,'');
+// 		try{
+// 			route[pathname](request,response);
+// 		}catch(error){
+// 			response.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
+// 			response.write(error.toString());
+// 			response.end();
+// 		}
+// 	}
+// });
+
+var app2=myExpress();
+app2.use((req,res,next)=>{
+	console.log('middleService1');
+	next();
+});
+app2.use((req,res,next)=>{
+	console.log('middleService2');
+	next();
+});
+app2.use('/test',(req,res,next)=>{
+	console.log('test');
 });
 
-app.listen(8888);
+http.createServer(app2).listen(3000);
---
+++ myExpress.js
@@ -0,0 +1,52 @@
+const url=require('url');
+function myExpress(){
+    var tasks=[];
+    var app=(request,response)=>{
+        extRequest(request);
+        extResponse(response);
+        var i=0;
+        function next(){
+            var task=tasks[i++];
+            if(!task){
+                return;
+            }
+            if(task.route===null || url.parse(request.url,true).pathname===task.route){
+                task.middleService(request,response,next);
+            }else{
+                next();
+            }
+        };
+        next();
+    };
+
+    app.use=(route,middleService)=>{
+        if(typeof route==='function'){
+            middleService=route;
+            route=null;
+        }
+        tasks.push(
+        {
+            route:route,
+            middleService:middleService
+        });
+    }
+
+    return app;
+}
+
+function extRequest(request){
+    request.query=url.parse(request.url,true).query;
+}
+function extResponse(response){
+    response.send=data=>{
+        if(typeof data==='string'){
+            response.end(data);
+        }else if(typeof data==='object'){
+            response.end(JSON.stringify(data));
+        }else if(typeof data==='number'){
+            response.end(data);
+        }
+    };
+}
+
+module.exports=myExpress;
